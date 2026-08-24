c520d249c06c8765be7645d98839ca4907a53c9a
inkuksuss <inkuksuss@naver.com>
2026-08-24 22:02:57 +0900

    init

---
+++ src/main/java/com/example/cinema_middleware/CinemaMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaMiddlewareApplication.class, args);
+	}
+
+}
